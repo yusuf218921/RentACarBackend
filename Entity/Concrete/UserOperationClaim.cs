c5d8097c82adb902012e8b7113a439be4cc57d42
Yusuf <yusufsonmez951@gmail.com>
2023-07-17 09:24:08 +0300

    JWT

---
+++ Entity/Concrete/UserOperationClaim.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class UserOperationClaim : IEntity
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int OperationClaimID { get; set; }
+    }
+}
